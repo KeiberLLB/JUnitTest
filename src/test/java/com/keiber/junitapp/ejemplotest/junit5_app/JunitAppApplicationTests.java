a858a40e104c583258a3abc8caee3b431e3ff97c
KeiberLLB <leandro1991.9@gmail.com>
2024-07-02 07:12:23 -0500

    first commit

---
+++ src/test/java/com/keiber/junitapp/ejemplotest/junit5_app/JunitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keiber.junitapp.ejemplotest.junit5_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
